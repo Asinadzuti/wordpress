d812f05cd79d112b7d4a328b50e2017b5de688fa
Asinadzuti <44137543+Asinadzuti@users.noreply.github.com>
2019-02-12 13:14:00 +0300

    row

--- wp-content/themes/woocomerceTheme/woocommerce/includes/wc-function.php
+++ wp-content/themes/woocomerceTheme/woocommerce/includes/wc-function.php
@@ -5,7 +5,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 // Change number or products per row to 3
 add_filter('loop_shop_columns', 'my_loop_columns', 99);
 function my_loop_columns() {
-		return 3; // 4 products per row
+		return 4; // 4 products per row
 }
 add_action( 'woocommerce_before_main_content', 'estore_add_sidebar_only_archive', 100 );
 function estore_add_sidebar_only_archive() {
