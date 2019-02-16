f4e78f6c5ca95642a937e2deae11c36e6de7416d
Asinadzuti <44137543+Asinadzuti@users.noreply.github.com>
2019-02-16 15:56:21 +0300

    fix

--- wp-content/themes/Woocommerce/templates/no-sidebar.php
+++ wp-content/themes/Woocommerce/templates/no-sidebar.php
@@ -11,30 +11,35 @@ get_header(); ?>
 
 					<?php
 
-					while ( have_posts() ) : the_post();
-						if ( is_order_received_page() ) :
-							?>
-							<div class="row">
-							<div class="col-md-6 col-md-offset-3">
-						<?php endif; ?>
-						<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-							<header class="entry-header">
-								<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-							</header><!-- .entry-header -->
+					while ( have_posts() ) : the_post();?>
 
-							<div class="entry-content">
+						<div class="row">
+						<?php if ( is_order_received_page() ) :?>
+							<div class="col-md-6 col-md-offset-3">
+								<?php endif; ?>
+						<?php if ( is_wc_endpoint_url('lost-password') ) :?>
+							<div class="col-md-5 col-md-offset-4">
+								<?php endif; ?>
+
+								<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+									<header class="entry-header">
+										<?php the_title( '<h1 class="entry-title margin-bottom-md">', '</h1>' ); ?>
+									</header><!-- .entry-header -->
+
+									<div class="entry-content">
+										<?php
+
+										the_content();
+										?>
+									</div><!-- .entry-content -->
+
+								</article><!-- #post-<?php the_ID(); ?> -->
 								<?php
 
-								the_content();
-								?>
-							</div><!-- .entry-content -->
-
-						</article><!-- #post-<?php the_ID(); ?> -->
-						<?php
-
 						if ( is_order_received_page() ) :
 							?>
-							</div></div>
+							</div>
+						</div>
 						<?php endif;
 					endwhile; // End of the loop.
 					?>
