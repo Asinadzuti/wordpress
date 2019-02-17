c4aa1c074ae4e1b7849904e49ee25c3a99fecfa3
Asinadzuti <44137543+Asinadzuti@users.noreply.github.com>
2019-02-17 20:50:46 +0300

    Archive

--- wp-content/themes/Woocommerce/woocommerce/includes/wc-function-archive.php
+++ wp-content/themes/Woocommerce/woocommerce/includes/wc-function-archive.php
@@ -31,10 +31,19 @@ function estore_archive_content_wrapper_end(){
 	</div>
 	<?php
 }
-
+remove_filter( 'woocommerce_product_loop_start', 'woocommerce_maybe_show_product_subcategories');
+add_action( 'woocommerce_before_shop_loop', 'estore_out_subcategories',40 );
+function estore_out_subcategories() {
+	$cat_out = woocommerce_output_product_categories( array(
+		'before'    => '<ul class="w3ls_mobiles_grid_right_grid2">',
+		'after'     => '<div class="clearfix"></div></ul>',
+		'parent_id' => is_product_category() ? get_queried_object_id() : 0,
+	) );
+	return $cat_out;
+}
 add_filter( 'product_cat_class', 'estore_add_classes_product_cat' );
 function estore_add_classes_product_cat($classes){
-	$classes[] = 'col-md-4';
+	$classes[] = 'col-md-4 agileinfo_new_products_grid agileinfo_new_products_grid_mobiles';
 return $classes;
 }
 add_filter( 'woocommerce_subcategory_count_html', 'estore_remove_count_categories' );
@@ -55,7 +64,7 @@ function estore_hide_title_shop( $hide ) {
 add_filter( 'post_class', 'estore_add_class_loop_item' );
 function estore_add_class_loop_item($clasess){
 	if(is_shop() || is_product_taxonomy()){
-		$clasess[] = 'col-md-4';
+		$clasess[] = 'col-md-4 agileinfo_new_products_grid agileinfo_new_products_grid_mobiles';
 	}
 
 	return $clasess;
@@ -65,7 +74,7 @@ add_filter( 'post_class', 'estore_add_class_loop_item_cross' );
 function estore_add_class_loop_item_cross($clasess){
 	if(is_cart()){
 		if ( in_array( 'product', $clasess ) ) {
-			$clasess[] = 'col-md-4';
+			$clasess[] = 'col-md-4 agileinfo_new_products_grid agileinfo_new_products_grid_mobiles';
 		}
 	}
 
@@ -78,12 +87,14 @@ remove_action( 'woocommerce_after_shop_loop_item', 'woocommerce_template_loop_pr
 add_action( 'woocommerce_before_shop_loop_item', 'estore_loop_product_div_open' , 5);
 function estore_loop_product_div_open(){
 	?>
+	<div class="mobiles_grid">
 	<?php
 }
 
 add_action( 'woocommerce_after_shop_loop_item', 'estore_loop_product_div_close' , 20);
 function estore_loop_product_div_close(){
 	?>
+	</div>
 	<?php
 }
 add_action( 'woocommerce_before_shop_loop_item_title', 'estore_loop_product_div_image_open', 5 );
