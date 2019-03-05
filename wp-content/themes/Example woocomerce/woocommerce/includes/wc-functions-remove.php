a5caf3677face757a28e62930cad91df5748b30a
Asinadzuti <44137543+Asinadzuti@users.noreply.github.com>
2019-03-05 17:24:31 +0300

    single

--- wp-content/themes/Example woocomerce/woocommerce/includes/wc-functions-remove.php
+++ wp-content/themes/Example woocomerce/woocommerce/includes/wc-functions-remove.php
@@ -10,6 +10,7 @@ remove_action( 'woocommerce_sidebar', 'woocommerce_get_sidebar', 10 );
 remove_action( 'woocommerce_before_shop_loop_item_title', 'woocommerce_show_product_loop_sale_flash', 10 );
 remove_action( 'woocommerce_before_main_content', 'woocommerce_breadcrumb', 20);
 remove_action( 'woocommerce_after_shop_loop', 'woocommerce_pagination', 10 );
+add_filter( 'wc_product_sku_enabled', '__return_false' );
 
 
 
